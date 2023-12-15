21a617d2fd2920ec16c5824299e9cf5e91461d31
Roy Jones Santoso <92863804+royjonessantoso1703@users.noreply.github.com>
2023-12-15 15:48:38 +0700

    Calculator Only

--- lib/pages/calculator.dart
+++ lib/pages/calculator.dart
@@ -6,11 +6,44 @@ class CalculatorWidget extends StatefulWidget {
 }
 
 class _CalculatorWidgetState extends State<CalculatorWidget> {
+  TextEditingController heightController = TextEditingController();
+  TextEditingController weightController = TextEditingController();
+  Color cardColor = Colors.transparent;
+  String result = "";
+
+  void calculateBMI() {
+    double height = double.parse(heightController.text);
+    double weight = double.parse(weightController.text);
+
+    // Formula BMI
+    double bmi = weight / ((height / 100) * (height / 100));
+
+    // Interpretation of BMI
+    String interpretation;
+    if (bmi < 18.5) {
+      interpretation = "Underweight";
+      cardColor = Colors.red;
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+      interpretation = "Normal weight";
+      cardColor = Colors.green;
+    } else if (bmi >= 25 && bmi < 29.9) {
+      interpretation = "Overweight";
+      cardColor = Colors.yellow;
+    } else {
+      interpretation = "Obesity";
+      cardColor = Colors.red;
+    }
+
+    setState(() {
+      result = "BMI: ${bmi.toStringAsFixed(2)}\n$interpretation";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Calculator"),
+        title: Text("BMI Calculator"),
       ),
       body: SingleChildScrollView(
         child: Center(
@@ -18,8 +51,41 @@ class _CalculatorWidgetState extends State<CalculatorWidget> {
             children: [
               Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Text("calculation"),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: heightController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(labelText: "Height (cm)"),
+                    ),
+                    TextField(
+                      controller: weightController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(labelText: "Weight (kg)"),
+                    ),
+                    SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: () {
+                        calculateBMI();
+                      },
+                      child: Text("Calculate"),
+                    ),
+                  ],
+                ),
               ),
+              result.isNotEmpty
+                  ? Card(
+                      margin: EdgeInsets.all(16),
+                      color: cardColor,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          result,
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    )
+                  : Container(),
             ],
           ),
         ),
@@ -27,4 +93,3 @@ class _CalculatorWidgetState extends State<CalculatorWidget> {
     );
   }
 }
-
